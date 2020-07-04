4ce79e5cb6d712aa57bc7c69af1da131b94b82ea
madhav.hugar <madhav.hugar@everestate.com>
2020-07-04 11:32:51 +0200

    define basic planet types

---
+++ src/main/java/com/example/javamavenjunithelloworld/Planet.java
@@ -0,0 +1,10 @@
+package com.example.javamavenjunithelloworld;
+
+
+public final class Planet {
+    public static final int HOME = 0;
+    public static final int ALLY = 1;
+    public static final int ENEMY = 2;
+    public static final int NEUTRAL = 3;
+    public static final int UNIVERSE_VOID = 10;
+}
