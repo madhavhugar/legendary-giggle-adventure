c40db1d72d29aa521fdad5b50475a4847efed010
madhavhugar <madhav.hugar@gmail.com>
2020-07-06 14:40:23 +0200

    separate common config into Config class

---
+++ src/main/java/com/example/javamavenjunithelloworld/Config.java
@@ -0,0 +1,6 @@
+package com.example.javamavenjunithelloworld;
+
+public class Config {
+    public static final String SAVE_GAME_FILE = "saved-adventure.ser";
+    public static final String INVALID_OPTION = "999";
+}
