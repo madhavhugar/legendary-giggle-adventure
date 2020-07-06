f42d00fcbfb684b79139ce23e59a3c4f333ddf12
madhavhugar <madhav.hugar@gmail.com>
2020-07-06 14:39:27 +0200

    include more test cases for Adventure

--- src/main/java/com/example/javamavenjunithelloworld/Adventure.java
+++ src/main/java/com/example/javamavenjunithelloworld/Adventure.java
@@ -1,11 +1,15 @@
 package com.example.javamavenjunithelloworld;
 
 
+import com.example.javamavenjunithelloworld.map.Position;
+import com.example.javamavenjunithelloworld.map.UniverseMap;
+import com.example.javamavenjunithelloworld.utilities.Screen;
+
 import java.io.*;
 
 
-public class Adventure implements Serializable {
-
+public class Adventure implements Serializable, AdventureInterface {
+    private static final long serialVersionUID = 101L;
     private Player player;
     private UniverseMap universeMap;
 
@@ -20,32 +24,80 @@ public class Adventure implements Serializable {
         // Update player position, health and experience
         updatePlayer(direction);
 
-        if (!player.isAlive()) {
-            return true;
-        }
-        return false;
+        return isAdventureOver();
     }
 
-    public void showPlayerVision() {
-        universeMap.showLocalVision(player.getCurrentPosition());
+    public String showPlayerVision() {
+        return universeMap.getLocalVision(player.getCurrentPosition());
     }
 
     public String getDescription() {
-        return player.getName() + "'s adventure";
+        return String.format("%s's adventure", player.getName());
     }
 
     public String getStatistics() {
         return player.getPlayerStats();
     }
 
-    public boolean save() throws IOException {
-        System.out.println("Saving adventure...\n");
-        FileOutputStream file = new FileOutputStream(Utilities.SAVE_GAME_FILE);
-        ObjectOutputStream out = new ObjectOutputStream(file);
-        out.writeObject(this);
-        out.close();
-        file.close();
-        System.out.println("Done.\n");
+    public boolean save(String filename) {
+        FileOutputStream file = null;
+        ObjectOutputStream out = null;
+        try {
+            System.out.println("Saving adventure...\n");
+            file = new FileOutputStream(filename);
+            out = new ObjectOutputStream(file);
+            out.writeObject(this);
+            System.out.println("Done.\n");
+        } catch (IOException | NullPointerException e) {
+            System.out.println("Could not save the game! " + e);
+        } finally {
+            try {
+                out.close();
+                file.close();
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+            return false;
+        }
+    }
+
+    public static Adventure load(String filename) {
+        FileInputStream file = null;
+        ObjectInputStream in = null;
+        File f = new File(filename);
+        if (!f.exists()) {
+            System.out.println("Could not find any saved adventure to resume... ");
+            return  null;
+        }
+        try {
+            file = new FileInputStream(filename);
+            in = new ObjectInputStream(file);
+            Adventure savedAdventure = (Adventure)in.readObject();
+            return savedAdventure;
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            try {
+                in.close();
+                file.close();
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void endGame() {
+        System.out.println("----------------------------------------------------------------------------------------");
+        System.out.println("\n\nthat was a quick ending to your adventure, perhaps you pushed it a little too far...");
+        System.out.println("\n\n \t\t\t xxx Game Over xxx\n\nPress any key to continue");
+        Screen.clearScreen(2);
+    }
+
+    private boolean isAdventureOver() {
+        if (!player.isAlive()) {
+            return true;
+        }
         return false;
     }
 
